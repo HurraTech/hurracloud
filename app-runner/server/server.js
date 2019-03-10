159ee2d9621f2abd4f8233043c514af4112344a6
aimannajjar <aiman.najjar@hurranet.com>
2019-03-09 23:06:40 -0500

    db

--- app-runner/server/server.js
+++ app-runner/server/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import HurraApp from './HurraApp/app';
+import sqlite3 from 'sqlite3';
+
+const db = null; // new sqlite3.Database('/var/hurradb/sqlite3.db');
+
 const server = express();
 const port = process.env.PORT || 5000;
 
 server.use(express.json())
 
-let app = new HurraApp(server)
+let app = new HurraApp(server, db)
 app.start()
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
