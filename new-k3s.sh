ef54fd917f892637d8f77c9749d692bb33538df9
aimannajjar <aiman.najjar@hurranet.com>
2020-01-12 16:45:10 -0500

    mounting usb bus

--- new-k3s.sh
+++ new-k3s.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-k3d create -a 172.16.0.99:6443 --volume ${HOME}/.k3d/config.toml.tmpl:/var/lib/rancher/k3s/agent/etc/containerd/config.toml.tmpl --volume /usr/share/hurracloud/mounts:/usr/share/hurracloud/mounts --publish 80:80 --publish 1024-10000:80
+k3d create -a 172.16.0.99:6443 \
+        --volume ${HOME}/.k3d/config.toml.tmpl:/var/lib/rancher/k3s/agent/etc/containerd/config.toml.tmpl\
+        --volume /usr/share/hurracloud/mounts:/usr/share/hurracloud/mounts\
+        --volume /dev/bus/usb:/dev/bus/usb \
+        --publish 80:80
+
