17d6c2e662201f4373bae80c3c68a771686cc7bd
Aiman Najjar <aiman.najjar@hurranet.com>
2020-08-17 14:45:36 -0400

    script enhancements

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-export HOSTNAME=$(hostname)
-MACHINE_TYPE=`uname -m`
+export MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64'  ]; then
     ARCH="amd64"
 else
     ARCH="arm64"
 fi
 
-export ARCH;
 
-docker-compose -f docker-compose.yml up -d
+HOSTNAME=$(hostname) ARCH=$ARCH docker-compose -f docker-compose.yml up -d
