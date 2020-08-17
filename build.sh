d428844bcc851b715122068173d288b95c5dec4c
aimannajjar <aiman.najjar@hurranet.com>
2020-08-17 19:19:29 -0400

    docker syncthing build

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ docker buildx build -f Jawhar.Dockerfile --platform linux/arm64,linux/amd64 -t a
 
 echo "Building Samaa"
 docker buildx build -f Samaa.Dockerfile --platform linux/arm64,linux/amd64 -t aimannajjar/hurracloud:samaa-v1 . --push
+
+echo "Building Syncthing"
+docker buildx build -f ./docker-syncthing/Dockerfile --platform linux/arm64,linux/amd64 -t aimannajjar/hurracloud:syncthing ./docker-syncthing --push
