fc865f3b8ea759374628a30b46dd1de97688bf95
aimannajjar <aiman.najjar@hurranet.com>
2020-08-17 14:24:30 -0400

    start script

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 export HOSTNAME=$(hostname)
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64'  ]; then
+    ARCH="amd64"
+else
+    ARCH="arm64"
+fi
+
+export ARCH;
+
 docker-compose -f docker-compose.yml up -d
