dbc7614fdf1324ed24fb989a85a9cccd734b6301
aimannajjar <aiman.najjar@hurranet.com>
2020-08-18 17:52:59 -0400

    rails prod

--- jawhar/config/initializers/resque.rb
+++ jawhar/config/initializers/resque.rb
@@ -1,5 +1,5 @@
 rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
-rails_env = ENV['RAILS_ENV'] || 'development'
+rails_env = ENV['RAILS_RESQUE_ENV'] || 'development'
 config_file = rails_root + '/config/resque.yml'
 
 resque_config = YAML::load(ERB.new(IO.read(config_file)).result)
@@ -10,7 +10,7 @@ Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
 
 Resque.after_fork do |job|
   if job.queue == "mounter"
-    Resque.logger.info("Initializing GRPC client for job #{job.inspect}") 
+    Resque.logger.info("Initializing GRPC client for job #{job.inspect}")
     $hurraAgent = ::Proto::HurraAgent::Stub.new('172.18.0.1:10000', :this_channel_is_insecure)
   end
 end
