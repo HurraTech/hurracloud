23b2e34d27fbbf8baa96081fc86a985801445d4c
aimannajjar <aiman.najjar@hurranet.com>
2020-08-18 15:17:27 -0400

    fixed api hostname

--- samaa/src/constants.js
+++ samaa/src/constants.js
@@ -1,3 +1,3 @@
 export const JAWHAR_API = "http://" + window.location.hostname + ":5000"
-export const APP_RUNNER_HOSTNAME = window.location.hostname
+export const APP_RUNNER_HOSTNAME = "http://" + window.location.hostname
 
