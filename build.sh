200a4c8c75a4c495ba74a54665630ac3ad8e1bd2
aimannajjar <aiman.najjar@hurranet.com>
2020-08-19 14:02:07 -0400

    apprunner build

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ docker buildx build -f Jawhar.Dockerfile --platform linux/arm64,linux/amd64 -t a
 echo "Building Samaa"
 docker buildx build -f Samaa.Dockerfile --platform linux/arm64,linux/amd64 -t aimannajjar/hurracloud:samaa-v1 . --push
 
+echo "Building AppRunner"
+docker buildx build -f AppRunner.Dockerfile --platform linux/arm64,linux/amd64 -t aimannajjar/hurracloud:app-runner-v1 . --push
+
 echo "Building Syncthing"
 docker buildx build -f ./docker-syncthing/Dockerfile --platform linux/arm64,linux/amd64 -t aimannajjar/hurracloud:syncthing ./docker-syncthing --push
 
