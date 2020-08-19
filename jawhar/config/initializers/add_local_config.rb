d0c5513fce4079969ddbe8088a121e93ed035c5b
aimannajjar <aiman.najjar@hurranet.com>
2020-08-19 19:54:09 -0400

    bug fix

--- jawhar/config/initializers/add_local_config.rb
+++ jawhar/config/initializers/add_local_config.rb
@@ -3,5 +3,5 @@ Settings.add_source!({skip_partitions: ENV['SKIP_PARTITIONS']})
 Settings.add_source!({samaa_root_url: ENV['SAMAA_ROOT_URL']})
 Settings.add_source!({host_app_store_path: ENV['HOST_APP_STORE_PATH']})
 Settings.add_source!({host_uid_: ENV['HOST_UID']})
-Settings.add_source!({hostname_: ENV['HURRA_HOSTNAME']})
+Settings.add_source!({hostname: ENV['HURRA_HOSTNAME']})
 Settings.reload!
