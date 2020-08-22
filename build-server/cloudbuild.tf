1332f72c8ae8f76998ca476cbe4c8848d13f38a5
aimannajjar <aiman.najjar@hurranet.com>
2020-08-22 04:03:19 -0400

    build triggers scope

--- build-server/cloudbuild.tf
+++ build-server/cloudbuild.tf
@@ -2,24 +2,29 @@ locals {
   projects = {
     "nginx-proxy" : {
       "dockerfile" : "Dockerfile",
-      "context" : "nginx-proxy"
+      "context" : "nginx-proxy",
+      "included_files" : ["nginx-proxy/**"]
     },
     "jawhar" : {
       "dockerfile" : "Jawhar.Dockerfile",
       "context" : ""
+      "included_files" : ["jawhar/**"]
     },
     "samaa" : {
       "dockerfile" : "Samaa.Dockerfile",
       "context" : ""
       "base_stage" : "build"
+      "included_files" : ["samaa/**"]
     },
     "app-runner" : {
       "dockerfile" : "AppRunner.Dockerfile",
       "context" : ""
+      "included_files" : ["app-runner/**"]
     },
     "syncthing" : {
       "dockerfile" : "Dockerfile",
       "context" : "docker-syncthing"
+      "included_files" : ["docker-syncthing/**"]
     }
   }
 }
@@ -32,12 +37,15 @@ resource "google_cloudbuild_trigger" "hurra-build" {
     branch_name = "^master$"
     repo_name   = "bitbucket_aimannajjar_hurracloud"
   }
-
+  included_files = each.value.included_files
+  ignored_files = [
+    "**/*.md",
+  ]
   substitutions = {
     _DOCKER_FILE = each.value.dockerfile
     _CONTEXT     = each.value.context
     _APP_NAME    = each.key
-    _BASE_STAGE  = lookup(each.value, "base_stage" , "")
+    _BASE_STAGE  = lookup(each.value, "base_stage", "")
   }
 
   filename = "cloudbuild/cloudbuild.yaml"
