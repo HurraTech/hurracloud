a31de5a8ae7057bc3a5f263e58bc75709991ce66
Aiman Najjar <aiman.najjar@hurranet.com>
2020-08-23 22:58:53 +0000

    cleanups

--- yocto/build-rpi3/upload-artifacts.sh
+++ yocto/build-rpi3/upload-artifacts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp tmp/deploy/images/raspberrypi4-64/core-image-base-raspberrypi4-64.sdimg  gs://hurrabuild-build/
 gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp tmp/deploy/images/raspberrypi4-64/core-image-base-raspberrypi4-64.mender  gs://hurrabuild-build/
-gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp tmp/deploy/images/raspberrypi4-64/core-image-base-raspberrypi4-64*.bmap  gs://hurrabuild-build/
+gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp tmp/deploy/images/raspberrypi4-64/core-image-base-raspberrypi4-64.sdimg.bmap  gs://hurrabuild-build/
