4e029102b04cd464f83ffdfdcbbf6f9167f856e7
Maan Najjar <maan.najjar@gmail.com>
2021-02-02 08:13:52 -0500

    Find computer card

--- src/mobile_app/lib/ui/onboarding/ServerPairScreen.dart
+++ src/mobile_app/lib/ui/onboarding/ServerPairScreen.dart
@@ -23,45 +23,116 @@ class ServerPairScreenState extends State<ServerPairScreen> {
         ),
         backgroundColor: Color(0xfff2f2f2),
         body: Padding(
-          padding: const EdgeInsets.only(left:20, right:20),
+          padding: const EdgeInsets.only(left: 20, right: 20),
           child: Center(
             heightFactor: 1,
             child: Column(
               children: [
                 SizedBox(height: 20),
-                SizedBox(width: double.infinity,
+                SizedBox(
+                  width: double.infinity,
                   child: Text("Ok, let’s set up your computer",
                       textAlign: TextAlign.start,
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)),
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 22)),
                 ),
                 SizedBox(height: 15),
                 Card(
                   child: Padding(
                     padding: const EdgeInsets.all(20),
-                    child: Column(children: [
-                      Text(
-                          "Hop over to your computer and open the following URL in your browser",
-                          style: TextStyle(color: Color(0xff666666), fontSize: 14,fontWeight: FontWeight.w300)),
-                      SizedBox(height: 15),
-                      SizedBox(
-                        width: double.infinity,
-                        child: Text(
-                            "https://www.hurra.com/desktop",
-                            textAlign: TextAlign.start,
-                            style: TextStyle(color: Color(0xff666666), fontSize: 14,fontWeight: FontWeight.bold)),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                          "Then follow the instructions on your computer to install Hurra cloud server",
-                          style: TextStyle(color: Color(0xff666666), fontSize: 14,fontWeight: FontWeight.w300)),
-
-                    ],),
+                    child: Column(
+                      children: [
+                        Text(
+                            "Hop over to your computer and open the following URL in your browser",
+                            style: TextStyle(
+                                color: Color(0xff666666),
+                                fontSize: 14,
+                                fontWeight: FontWeight.w300)),
+                        SizedBox(height: 15),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Text("https://www.hurra.com/desktop",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                  color: Color(0xff666666),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                            "Then follow the instructions on your computer to install Hurra cloud server",
+                            style: TextStyle(
+                                color: Color(0xff666666),
+                                fontSize: 14,
+                                fontWeight: FontWeight.w300)),
+                      ],
+                    ),
                   ),
                 ),
                 SizedBox(
                   height: 30,
                 ),
+                Expanded(
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          FractionallySizedBox(
+                              widthFactor: 0.5,
+                              child: Image(
+                                  image:
+                                      AssetImage('assets/findcomputer.png'))),
+                          SizedBox(height: 15),
+                          Text("Let's find your computer",
+                              style: TextStyle(
+                                  color: Color(0xff666666),
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold)),
+                          SizedBox(height: 15),
+                          Text(
+                              "After installing Hurra on your computer, tap the button below to start searching"
+                              " for your computer. We need your permission to search in your network",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Color(0xff666666),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w300)),
+                          SizedBox(height: 20),
+                          SizedBox(
+                            width:double.infinity,
+                            child: RaisedButton(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18.0),
+                                ),
+                                color: hurraColor,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 12, left: 16, right: 16, bottom: 12),
+                                  child: Text("Start Searching"),
+                                ),
+                                textColor: Colors.white,
+                                onPressed: () {}),
+                          ),
+                          SizedBox(height: 15),
+                          FlatButton(
+                              onPressed: () {},
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                              ),
+                              textColor: Colors.lightBlue,
+                              splashColor: Colors.white10,
+                              child: Text("Tap here if you prefer to connect manually", style:TextStyle(fontWeight:FontWeight.normal), textAlign: TextAlign.center)),
 
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
               ],
             ),
           ),
--- src/mobile_app/lib/ui/onboarding/SplashScreen.dart
+++ src/mobile_app/lib/ui/onboarding/SplashScreen.dart
@@ -11,7 +11,6 @@ class SplashScreen extends StatefulWidget {
 }
 
 class SplashScreenState extends State<SplashScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,23 +20,33 @@ class SplashScreenState extends State<SplashScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             SizedBox(height: 100),
-            FractionallySizedBox(widthFactor: 0.65, child: Image(image: AssetImage('assets/logo.png'))),
+            FractionallySizedBox(
+                widthFactor: 0.65,
+                child: Image(image: AssetImage('assets/logo.png'))),
             SizedBox(height: 30),
             Expanded(
-              flex:1,
+              flex: 1,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                Text("TAKE BACK CONTROL", style: TextStyle(color: Colors.black, fontSize: 17.0, fontWeight: FontWeight.bold)),
-                Padding(
-                  padding: const EdgeInsets.only(top:17.0,left:40, right:40.0),
-                  child: Text("Hurra let’s you take back control of your data and "
-                      "privacy by creating your own private cloud which hosts "
-                      "your data and apps right inside your home",
-                      style: TextStyle(fontSize: 15.0, color: Color(0xff666666)),
-                      textAlign: TextAlign.center),
-                ),
-              ],),
+                  Text("TAKE BACK CONTROL",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.bold)),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 17.0, left: 40, right: 40.0),
+                    child: Text(
+                        "Hurra let’s you take back control of your data and "
+                        "privacy by creating your own private cloud which hosts "
+                        "your data and apps right inside your home",
+                        style:
+                            TextStyle(fontSize: 15.0, color: Color(0xff666666)),
+                        textAlign: TextAlign.center),
+                  ),
+                ],
+              ),
             ),
             // Spacer(flex:1),
             Expanded(
@@ -47,44 +56,47 @@ class SplashScreenState extends State<SplashScreen> {
                 child: Stack(
                   fit: StackFit.expand,
                   children: [
-                  Align(alignment: Alignment.bottomCenter, child:
-                  Image(
-                    fit: BoxFit.cover,
-                      alignment: AlignmentDirectional.topCenter,
-                      width: double.infinity,
-                      image: AssetImage('assets/splashbg.png'))
-                  ),
-                  // RaisedButton(child: Text("ss"),onPressed: () {})
-                  Column(
-                    children: [
-                      Spacer(flex:1),
-                      RaisedButton(
-                          shape: RoundedRectangleBorder(
+                    Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Image(
+                            fit: BoxFit.cover,
+                            alignment: AlignmentDirectional.topCenter,
+                            width: double.infinity,
+                            image: AssetImage('assets/splashbg.png'))),
+                    Column(
+                      children: [
+                        Spacer(flex: 1),
+                        RaisedButton(
+                            shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(18.0),
-                          ),
-                          color: Colors.white,
-                        child: Padding(
-                          padding: const EdgeInsets.only(top:12,left:16,right:16, bottom: 12),
-                          child: Text("SET UP NEW HURRA CLOUD"),
-                        ), textColor: hurraMainColor, onPressed: () {
-                            Navigator.push(
-                              context,
-                              CupertinoPageRoute(
-                                  builder: (context) => ServerTypeScreen()),
-                            );
-                      }
-                        ),
-                      SizedBox(height:5),
-                      FlatButton(onPressed: (){},
-                          shape: RoundedRectangleBorder(
+                            ),
+                            color: Colors.white,
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 12, left: 16, right: 16, bottom: 12),
+                              child: Text("SET UP NEW HURRA CLOUD"),
+                            ),
+                            textColor: hurraMainColor,
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                    builder: (context) => ServerTypeScreen()),
+                              );
+                            }),
+                        SizedBox(height: 5),
+                        FlatButton(
+                            onPressed: () {},
+                            shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(18.0),
-                          ),
-                          textColor: Colors.white,
-                          splashColor: Colors.white10,
-                          child: Text("Login to my existing Hurra Cloud ")),
-                      SizedBox(height: 100)],
-                  ),
-                ],
+                            ),
+                            textColor: Colors.white,
+                            splashColor: Colors.white10,
+                            child: Text("Login to my existing Hurra Cloud ")),
+                        SizedBox(height: 100)
+                      ],
+                    ),
+                  ],
                 ),
               ),
             )
