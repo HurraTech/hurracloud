502d41555a6d22b0907fb55343332e19b59bf2a2
Aiman Najjar <aiman@hurracloud.io>
2024-07-14 11:19:28 +0300

    clenaup hardware stuff

--- gcp/build-server/download-artifacts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp gs://hurrabuild-build/core-image-*.sdimg ./
-gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp gs://hurrabuild-build/core-image-*bmap ./
-
--- yocto/meta-hurracloud/recipes-hurracloud/hurracloud/avahi_%.bbappend
+++
@@ -1,6 +0,0 @@
-FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
-SRC_URI_append = " file://avahi-daemon.conf"
-do_install_append() {
-    install ${WORKDIR}/avahi-daemon.conf ${D}${sysconfdir}/avahi/avahi-daemon.conf
-}
-
